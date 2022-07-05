ac3aa5c1b0a19f58156c28dc199c06b3fedbf577
yumiryi <yumi.r.yi@gmail.com>
2022-07-04 23:46:11 -0400

    changed to react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PartyTimeline from './Timeline';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>FFXIV Mitigation Planner</h1>
+      <h3>Asphodelos: The Third Circle | P3S</h3>
+      <PartyTimeline />
+    </>
+  );
+}
+
+export default App;
